6e77c829561bf232f4c0f4302c96dc106543a9aa
H S <hannah.snyderburn@gmail.com>
2024-08-19 11:33:43 -0400

    began training essentials

---
+++ EssentialsCpp/toolchain.cpp
@@ -0,0 +1,6 @@
+// Development Cycle
+
+// Source Code to Preprocessor to Compiler to Linker to Run to Source Code
+
+//Linker - takes object files, takes interdependencies, and turns it into something OS can run. 
+//output from Linker is .exe
