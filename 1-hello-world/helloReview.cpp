5091eece45222ef323edda89b682c418aac48a5c
H S <hannah.snyderburn@gmail.com>
2024-08-19 11:29:53 -0400

    linkedIn trainings folders created and developed

---
+++ 1-hello-world/helloReview.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+
+    std::cout << "Dear Me,\n";
+    std::cout << "Who knows what will come of learning Cpp\n";
+    std::cout << "I hope there's plenty for me to do with it\n";
+    std::cout << "Let's try to get good.\n";
+
+}
